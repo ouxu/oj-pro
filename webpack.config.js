4474911f35d04f3fb3a7f5cf96fc609eebac9774
ouxu <out_xu@outlook.com>
2018-08-07 00:12:22 +0800

    💚 修改 CI 配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
 /**
  * Created by out_xu on 17/7/17.
  */
-const path = require('path')
 const QiniuPlugin = require('qiniu-webpack-plugin')
 module.exports = function (webpackConfig) {
   if (process.env.NODE_ENV === 'production') {
-    const qiniu = process.env.ci ? {
-      ACCESS_KEY: process.env.ACCESS_KEY || '',
-      SECRET_KEY: process.env.SECRET_KEY || '',
-      bucket: process.env.qiniu_bucket || '',
-      path: process.env.qiniu_path || ''
-    } : require('./pushConfig').qiniu
+    const qiniu = require('./pushConfig').qiniu
     webpackConfig.plugins.push(new QiniuPlugin(qiniu))
   }
   return webpackConfig
