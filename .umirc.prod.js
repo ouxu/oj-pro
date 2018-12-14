e2ed703f4429fe8e7504b15a15a167245c6770bd
ouxu <out_xu@outlook.com>
2018-12-14 16:50:21 +0800

    :wrench: 删除 browserslist 配置

--- .umirc.prod.js
+++ .umirc.prod.js
@@ -3,7 +3,6 @@ const QiniuPlugin = require('qiniu-webpack-plugin')
 export default {
   hash: true,
   targets: { ie: 10 },
-  browserslist: ['> 1%', 'last 2 versions', 'iOS >= 8'],
   publicPath: 'http://ojcdn.acmclub.cn/new/',
   chainWebpack: (config, { webpack }) => {
     config.optimization.delete('splitChunks')
