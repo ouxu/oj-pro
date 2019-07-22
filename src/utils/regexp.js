d96a2d71bf633486e3ba02b2c8db4505d4736aa1
风乘 <fengcheng.xjj@alibaba-inc.com>
2019-07-22 14:53:49 +0800

    :bug: mobile regexp

--- src/utils/regexp.js
+++ src/utils/regexp.js
@@ -6,7 +6,7 @@
  * some useful regexp
  */
 export default {
-  mobile: /^1[3|4|5|7|8][0-9]\d{8}$/,
+  mobile: /^1(3|4|5|6|7|8|9)\d{9}$/,
   password: /^\w{6,18}$/,
   mail: /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/,
   qq: /[1-9][0-9]{4,}/,
